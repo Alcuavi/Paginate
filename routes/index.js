7c0f3e9979dc5bf9bad7afdb6da34a43c428731b
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2018-12-18 12:44:39 +0100

    Paginacion en formato tabla

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var PagController = require('../controllers/pagController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let page = (parseInt(req.query.page)||1) -1;
-  let limit = 50;
+  let limit = 10;
   let offset= page * limit;
 
   pagController = new PagController(req, res, next);
